03abc4053bab52c572629ab7889fb3556b7c95f1
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-27 10:21:43 +0200

    validate pin code

---
+++ validate-pin-code.js
@@ -0,0 +1,11 @@
+function solve(pin){
+
+    //return /^(\d{4}|\d{6})$/.test(pin)
+    console.log(/^(\d{4}|\d{6})$/.test(pin));
+    
+
+}
+solve('1234');
+solve('12345');
+solve('a234');
+solve('123456');
