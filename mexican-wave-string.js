93caacd6d188e9c457dd0320c1d14ca037eb552a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-04-28 21:38:51 +0200

    mexican wave -V1

---
+++ mexican-wave-string.js
@@ -0,0 +1,18 @@
+function solve(str){
+
+    // let arr = str.split('');
+    // let num = str.length;
+    // let newArr = [];
+    // for ( let i =0; i < arr.length; i++){
+    //     for( let j =0; j <=num; j++){
+            
+    //     }
+    //     newArr.push(arr[i].charAt().toUpperCase());
+    //     console.log(newArr);
+    // }
+   
+    
+    
+
+}
+solve('hello')
