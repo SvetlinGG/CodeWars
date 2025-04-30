86f228ce469e76bdbdafdf49d834743432d54b83
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-01 00:38:34 +0200

    bakers

--- the-baker.js
+++ the-baker.js
@@ -1,6 +1,20 @@
-function solve(obj){
-    let obj
+function solve(recipe, materials){
+
+    let recipeProduct = Object.keys(recipe);
+    let materialsProduct = Object.keys(materials);
+
+    for ( let item1 of recipeProduct){
+        for ( let item2 of materialsProduct){
+            if ( item1 === item2){
+                console.log(item1);
+                
+            }
+        }
+    }
+    
+    
+    
 
 
 }
-solve
+solve({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})
