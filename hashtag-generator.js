ad930f07f90bcb51f6c9b07984d39a3411ddb407
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-01 22:24:34 +0200

    hashtag generator

---
+++ hashtag-generator.js
@@ -0,0 +1,12 @@
+function hashtag(str){
+    if (!str || str.trim() === '') return false;
+    let arr = str.split(/\s+/);
+    
+    let hashtag = '#' + arr.map(word =>  word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+     
+    return hashtag.length > 140 ? false : hashtag ;
+    
+
+}
+hashtag(" Hello there thanks for trying my Kata");
+hashtag("   Hello     World   ");
