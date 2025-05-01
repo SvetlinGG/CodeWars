91d499f3ee1e020b1a3ad3588e56b8522cc28d14
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-01 23:52:38 +0200

    permutation

---
+++ permutation.js
@@ -0,0 +1,12 @@
+function permutation(str){
+
+    let arr = str.split('');
+    for ( let i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+        
+    }
+    
+
+
+}
+permutation('abc')
