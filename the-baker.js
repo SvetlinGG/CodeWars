9ca69521247f23a0566ce63ab87277ffbc8e1a8e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-01 09:35:43 +0200

    the bakers vol-2

--- the-baker.js
+++ the-baker.js
@@ -1,20 +1,26 @@
 function solve(recipe, materials){
 
-    let recipeProduct = Object.keys(recipe);
-    let materialsProduct = Object.keys(materials);
-
-    for ( let item1 of recipeProduct){
-        for ( let item2 of materialsProduct){
-            if ( item1 === item2){
-                console.log(item1);
+    let recipeProduct = Object.entries(recipe);
+    let materialsProduct = Object.entries(materials);
+    let result ;
+    let arr = [];
+    
+    for ( let [item1, item2] of recipeProduct){
+        for (let [item11, item22] of materialsProduct){
+            if (item1 === item11){
+        
+                if ( item22 > item2){
+                    result = item22 / item2;
+                    arr.push(result);
+                    
+                }
                 
             }
         }
     }
-    
-    
-    
-
-
+        arr.sort((a, b) => a - b);
+        console.log(Math.floor(arr[0]));
+   
 }
-solve({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})
+solve({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+solve({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000})
