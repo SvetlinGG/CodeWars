1a669bb4ef8dea76766564504ef785e095007ca7
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-05-02 13:47:18 +0200

    permutation

--- permutation.js
+++ permutation.js
@@ -1,12 +1,19 @@
 function permutation(str){
 
-    let arr = str.split('');
-    for ( let i = 0; i < arr.length; i++){
-        console.log(arr[i]);
-        
+    if( str.length <= 1){
+        console.log([str]); 
     }
+    let arr = [];
+    for ( let i = 0; i < str.length; i++){
+        let char = str[i];
+        let remaining = str.slice(0, i) + str.slice(i + 1);
+        console.log(remaining);
+        
+        for ( let perm of remaining){
+            arr.push(char+perm);
+        }
+    }
+    //console.log(arr.join(' '));
     
-
-
 }
 permutation('abc')
