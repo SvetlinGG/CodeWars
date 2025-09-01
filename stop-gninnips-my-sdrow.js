59699570550527452d75164c7b65118044fe3a6e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-01 22:49:39 +0200

    setup words

--- stop-gninnips-my-sdrow.js
+++ stop-gninnips-my-sdrow.js
@@ -1,26 +1,12 @@
 function reverseWord(str){
     let words = str.split(' ');
-    let arr = []
-
     words.map(word => {
         if ( word.length >= 6){
-            word.split('').reverse().join('')
+            word.split('').reverse();
             
         }
-        console.log(words);
-    });
+    })
+    console.log(words.join(' '));
     
-    
-    // for ( let word of words){
-    //     if ( word.length >= 6){
-    //         word = word.split('').reverse();   
-    //     }
-    //     arr.push(word)
-        
-    // }
-    // console.log(arr.join(' '));
-    
-    
-
 }
 reverseWord('Hey fellow warriors');
