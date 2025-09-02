dce29f0f77ffec5bd3b7d4cd4d3cccebb70fc94a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-03 00:14:30 +0200

    lower and upper case

--- convert-to camel-case.js
+++ convert-to camel-case.js
@@ -1,9 +1,14 @@
 function convert(str){
 
     
-    
-    let result = str.match(/[a-zA-Z]+/);
-    console.log(result);
+    let sentence = '';
+    let result = str.match(/[a-zA-Z]+/g);
+    for ( let word of result){
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+        sentence += word;
+        
+    }
+    console.log(sentence);
     
 }
 convert("the-stealth-warrior");
