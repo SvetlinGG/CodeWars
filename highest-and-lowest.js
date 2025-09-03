12984717b35bb068a68f5cdfae145efa0be3f60a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-03 23:57:48 +0200

    new decision

--- highest-and-lowest.js
+++ highest-and-lowest.js
@@ -1,21 +1,10 @@
 function highestLowest(num){
 
-    let arr = num.split(' ');
-    arr = arr.map(Number);
-    let sortedArr = arr.sort((a, b) => a - b);
-    let highest = sortedArr.pop();
-    let lowest = sortedArr.shift();
-    let newArr = [];
-    newArr.push(highest);
-    newArr.push(lowest);
-    console.log(newArr);
-    
-    
-    
-
-    
-    
-
-
+    let arr = num.split(' ').map(Number);
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    console.log(`${max} ${min}`);
+  
 }
 highestLowest("1 2 -3 4 5");
+highestLowest("1 9 3 4 -5")
