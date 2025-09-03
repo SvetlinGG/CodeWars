efb571a8c632f3a5cfaaf7461c0c47152725ecc9
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-03 15:41:25 +0200

    setup code

--- cfredit-card-mask.js
+++ cfredit-card-mask.js
@@ -1,8 +1,18 @@
 function maskify(cc) {
 
     let code = cc.split('');
-    console.log(code);
-    
-
+    let lastItems = code.slice(-4).join('');
+    let arr = [];
+    for (let i= 0; i < code.length - 4; i ++){
+        let symbol = code[i].replace(code[i], '#');
+        arr.push(symbol);
+    }
+    arr.push(lastItems)
+    console.log(arr.join(''));
+  
 }
 maskify("4556364607935616");
+maskify("64607935616");
+maskify('1');
+maskify('Skippy');
+maskify('')
