3244bec41a24b7c360e1b55725db7b59675118a0
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-04 23:25:20 +0200

    split b

--- array-diff.js
+++ array-diff.js
@@ -1,9 +1,11 @@
 function arrayDiff(a, b) {
-    b = b.join('')
+    b = b.join(',')
     let replaced = a.map(n => n == b ? '' : n);
-    console.log(replaced.join('').split('').map(Number));
+    //console.log(replaced.join('').split('').map(Number));
+    console.log(b);
     
   
 }
 arrayDiff([1, 2], [1]);
 arrayDiff([1, 2, 2, 2, 3], [2]);
+arrayDiff([1, 2, 3], [1, 2,3]);
