3a4914619f88c65a95f822599dad6ef4d07d78fd
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-09-19 23:35:37 +0200

    extract the domain name

--- extract-domain-name.js
+++ extract-domain-name.js
@@ -1,22 +1,5 @@
-function solve(str){
-
-    let word = str.split('/');
-    let word2 = word[2].split('.com')
-    let word3 = word2.join(' ');
-    let word4 = word3.split('.');
-    if ( word4.length === 1){
-        console.log(word4[0]);
-        
-    }else {
-        console.log(word4[1]);
-    }
+function solve(url){
     
-    
-    
-    
-    
-    
-
 
 }
 solve("http://github.com/carbonfive/raygun");
