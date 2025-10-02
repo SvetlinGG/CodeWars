2c2d9000406a7edd3da0d0b863a09d161afee3c8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-02 23:24:58 +0200

    first successful catch

--- extract-domain-name.js
+++ extract-domain-name.js
@@ -1,9 +1,11 @@
 function solve(url){
-    let pattern = /^(?:https?:\/\/|w+)?([a-z]+|W)(.[a-z]+)/
+    let pattern = /^(?:https?:\/\/|w+)?(www\.)?/
+    
     let match = url.match(pattern);
     
-    //console.log(match[0]);
-    console.log(match[1]);
+    console.log(match[0]);
+    console.log(match);
+    
     
     
     
