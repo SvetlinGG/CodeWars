48f30b6ebe16c511e06644e2a7f511d4ca01d37d
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-03 23:08:00 +0200

    hashtag string

--- strings/the-hashtag-generator.js
+++ strings/the-hashtag-generator.js
@@ -1,5 +1,12 @@
 function generateHashtag(str){
 
+    let arr = ['#'];
+    for (let char of str){
+        arr.push(char);
+    }
+    console.log(arr.join(''));
+    
+
 
 }
 generateHashtag(" Hello there thanks for trying my Kata");
