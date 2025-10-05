1560dd13cfa235bafb99b0ec580ed39c6586d559
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-05 23:28:50 +0200

    stuck

--- strings/the-hashtag-generator.js
+++ strings/the-hashtag-generator.js
@@ -2,8 +2,8 @@ function generateHashtag(str){
 
     let arr = ['#'];
     let pattern = /\s+/g;
-    str = str.replace(pattern, '');
-    for (let i =0; i < str.length; i++){
+    //str = str.replace(pattern, '');
+    for (let i = 0; i < str.length; i++){
         let char = (i === 0) ? str[i].toUpperCase() : str[i];
         arr.push(char)
     }
