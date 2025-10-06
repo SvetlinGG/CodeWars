9c2e2286a2f4f590eaa347da535aea4c6699db96
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-06 19:29:23 +0200

    step up

--- strings/the-hashtag-generator.js
+++ strings/the-hashtag-generator.js
@@ -14,5 +14,5 @@ function generateHashtag(str){
 
 }
 generateHashtag(" Hello there thanks for trying my Kata");
-//generateHashtag("   Hello     World   ");
+generateHashtag("   Hello     World   ");
 //generateHashtag("");
