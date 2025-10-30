548aa4ff17d90309a6b4d54bb703b6f57d349d0b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-30 07:24:52 +0100

    tic tac toe

---
+++ algorithms/tic-tac-toe-checker.js
@@ -0,0 +1,11 @@
+function isSolved(board){
+
+    console.log(board[0]);
+    console.log(board[1]);
+    console.log(board[2]);
+    
+
+}
+isSolved([[0, 0, 1],
+          [0, 1, 2],
+          [2, 1, 0]]);
