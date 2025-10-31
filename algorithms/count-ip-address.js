21b9aaf38853a765b09e18c425a1f7558611082a
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-31 12:22:19 +0100

    count ip address

--- algorithms/count-ip-address.js
+++ algorithms/count-ip-address.js
@@ -1,6 +1,12 @@
-function ipsBetween(start, end){
-    
-  }
+function ipsBetween(start, end) {
+  // helper: convert IP string to integer
+  const ipToNum = ip => 
+    ip.split('.')
+      .reduce((acc, part) => acc * 256 + Number(part), 0);
+
+    console.log(ipToNum(end) - ipToNum(start));
+}
+
 
   ipsBetween("10.0.0.0", "10.0.0.50");
   ipsBetween("10.0.0.0", "10.0.1.0");
