00923ac325ee8216c0c5d7892a2c9e1c50db911c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-31 12:50:17 +0100

    many permutations

---
+++ algorithms/many-permutations.js
@@ -0,0 +1,20 @@
+function solve(str){
+
+   if ( str.length === 1){
+    console.log([str]);
+   }
+   let arr = [];
+   for ( let i = 0; i < str.length; i ++){
+    let char = str[i];
+    let rest = str.slice(0, i) + str.slice(i + 1);
+
+    for ( let perm of rest){
+        arr.push(char + perm)  
+    }
+   }
+   console.log(arr);
+   
+}
+solve('a')
+solve('ab')
+solve('aabb')
