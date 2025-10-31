113cf3276c725aaa887d071df81fad2afec8b9d1
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-10-31 12:08:16 +0100

    merge strings finall

--- algorithms/merge-strings.js
+++ algorithms/merge-strings.js
@@ -1,17 +1,29 @@
 function mergeStrings(word1, word2){
 
-    let newStr1 = word1.split('');
-    let newStr2 = word2.split('');
-    let result = '';
+    // let newStr1 = word1.split('');
+    // let newStr2 = word2.split('');
+    // let arr = [];
+    // let result = ''
 
-    for ( let i = 0; i < newStr1.length; i ++){
-        result += newStr1[i] + newStr2[i];
+    // for ( let i = 0; i < newStr1.length; i ++){
+    //     arr.push(newStr1[i] + newStr2[i]);
+    //     if (newStr1.length > newStr2.length || newStr1.length < newStr2.length){
+    //         result  += newStr2.length - newStr1.length;
+    //     }
+    // }
+    // arr.push(result)
+    // console.log(arr.join(''));
 
-    }
-    console.log(result);
+    let arr = [];
+    let maxLength = Math.max(word1.length, word2.length);
+
+    for (let i = 0; i < maxLength; i++) {
+        if (word1[i]) arr.push(word1[i]);
+        if (word2[i]) arr.push(word2[i]);
+  }
+
+  console.log(arr.join(''));
+  return arr.join('');
     
-    
-
-
 }
-mergeStrings('abc', 'pqr');
+mergeStrings('abc', 'pqraa');
