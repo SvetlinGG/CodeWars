f899e5cf57b0a0c56d25e4146b4b2638fd3c7b91
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-11-01 07:01:41 +0100

    permutation

--- algorithms/many-permutations.js
+++ algorithms/many-permutations.js
@@ -1,13 +1,15 @@
 function solve(str){
 
-   if ( str.length === 1){
-    console.log([str]);
-   }
+   
    let arr = [];
    for ( let i = 0; i < str.length; i ++){
+    if ( str.length === 1){
+        console.log([str]);
+       }
     let char = str[i];
     let rest = str.slice(0, i) + str.slice(i + 1);
-
+       console.log(rest);
+       
     for ( let perm of rest){
         arr.push(char + perm)  
     }
