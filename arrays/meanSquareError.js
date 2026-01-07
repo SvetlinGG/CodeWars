832ed3228daf3f2d6b84c839f46d44af2babb143
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-07 16:21:03 +0200

    solved task

--- arrays/meanSquareError.js
+++ arrays/meanSquareError.js
@@ -3,6 +3,7 @@ function meanSquare(arr1, arr2){
 
     let result ;
     let newArr = [];
+    let sum = 0;
 
     for ( let i = 0; i < arr1.length; i++){
             if ( arr1 !== arr2){
@@ -12,9 +13,18 @@ function meanSquare(arr1, arr2){
         }
     }
     newArr = newArr.map(n => Math.pow(n, 2))
-    console.log(newArr);
+    let length = newArr.length;
+    for ( let num of newArr){
+        sum += num;
+    }
+    let newResult = sum / length
+    console.log(newResult);
+    
+    
     
 
 }
-meanSquare([1, 2, 3], [4, 5, 6])
+meanSquare([1, 2, 3], [4, 5, 6]);
+meanSquare([10, 20, 10, 2], [10, 25, 5, -2]);
+meanSquare([-1, 0], [0, -1])
 
