9d488f79b7610f444b7394f52bbdca5196eed730
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-07 16:15:15 +0200

    set the real value

--- arrays/meanSquareError.js
+++ arrays/meanSquareError.js
@@ -5,14 +5,13 @@ function meanSquare(arr1, arr2){
     let newArr = [];
 
     for ( let i = 0; i < arr1.length; i++){
-
-        for ( let j = 0; j < arr2.length; j++){
-            if ( arr1[i] !== arr2[j]){
-                result = arr1[j] - arr2[i];
+            if ( arr1 !== arr2){
+                result = arr1[i] - arr2[i];
                 newArr.push(result);
-            }
+            
         }
     }
+    newArr = newArr.map(n => Math.pow(n, 2))
     console.log(newArr);
     
 
