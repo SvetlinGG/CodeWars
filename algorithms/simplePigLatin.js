8302bcdef57458206d148898e8a0e0f83a79ec84
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-09 23:55:26 +0100

    setup file

--- algorithms/simplePigLatin.js
+++ algorithms/simplePigLatin.js
@@ -2,7 +2,7 @@ function pigIt(str){
 
     let pattern = '/([a-z,A-Z]+)/';
     let char = 'ay';
-    let word = str.split(' ').map(w => w.slice(0, 1));
+    let word = str.split(' ').map(w => w.slice(0));
     console.log(word);
     
     
