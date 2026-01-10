85ff2c7a518cc2974a46ac027246519f3fd79db3
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-10 23:09:48 +0100

    final decision

--- algorithms/simplePigLatin.js
+++ algorithms/simplePigLatin.js
@@ -1,20 +1,34 @@
-function pigIt(str){
+function pigIt(str) {
+    return str
+      .split(" ")
+      .map(word => {
+        if (!/[a-zA-Z]/.test(word)) return word; 
+        return word.slice(1) + word[0] + "ay";
+      })
+      .join(" ");
+  }
+  
+  console.log(pigIt("Pig latin is cool"));
 
-    //let pattern = '/([a-z,A-Z]+)/';
+
+// function pigIt(str){
+
+//     //let pattern = '/([a-z,A-Z]+)/';
     
-    let char = 'ay';
-    let word = str.split(' ');
-
-
-    for ( let i = 0; i < word.length; i++){
-        let newArr = Array.from(word[i])
-        //newArr.map( w => w.splice())
-        console.log(newArr);
+//     let char = 'ay';
+//     let word = str.split(' ').map( w => {
+//         if (!/[a-zA-Z]/.test(word)) {
+//             return word;
+//         }
+//         return w.slice(1) + w[0] + char;
         
-    }
-
+//     })
+//     console.log(word.join(' '));
     
     
     
-}
-pigIt('Pig latin is cool')
+    
+    
+// }
+// pigIt('Pig latin is cool');
+// pigIt('Hello world !');
