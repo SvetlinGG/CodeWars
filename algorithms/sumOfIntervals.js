7871da6542563d2181af4c4d830d8ae6cad85183
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-11 21:44:27 +0100

    set data

--- algorithms/sumOfIntervals.js
+++ algorithms/sumOfIntervals.js
@@ -1,6 +1,7 @@
 function sumIntervals(intervals){
 
     let arr = [];
+    let sum = 0;
 
     for ( let i = 0; i < intervals.length; i++){
         let firs = intervals[i][0];
@@ -8,13 +9,29 @@ function sumIntervals(intervals){
         let diff = second - firs;
         arr.push(diff);
     }
-    console.log(arr);
+    for (let num of arr){
+        sum += num
+    }
+    console.log(sum);
+    
     
 
 
 }
-sumIntervals([
-    [1, 4],
+// sumIntervals([
+//     [1, 4],
+//     [7, 10],
+//     [3, 5]]);
+ sumIntervals([[1, 2],
+    [6, 10],
+    [11, 15]]);
+sumIntervals([[1, 4],
     [7, 10],
     [3, 5]
+ ]);
+sumIntervals([[1, 5],
+    [10, 20],
+    [1, 6],
+    [16, 19],
+    [5, 11]
  ])
