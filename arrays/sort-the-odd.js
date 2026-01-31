bec3405153cfaa31a40a2af8f97b9683518a2694
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-31 15:37:31 +0100

    nice decision

--- arrays/sort-the-odd.js
+++ arrays/sort-the-odd.js
@@ -1,20 +1,18 @@
 function sortArray(arr) {
 
    let odd = arr.filter( n => n % 2 !== 0).sort((a, b) => a - b);
-   console.log(odd);
    let i = 0;
 
-   for ( let num of arr){
-
+   arr = arr.map( num => {
     if ( num % 2 !== 0){
       const v = odd[i];
       i ++;
       console.log(v);
-      
     }
     console.log(num);
-   }
+    
+   })
    
   }
   sortArray([5, 8, 6, 3, 4])
-  sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+  //sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
