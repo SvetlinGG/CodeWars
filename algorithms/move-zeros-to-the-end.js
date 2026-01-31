daa61caf22ca256fc4b1ba4bffd2dc4f8bb23117
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-31 11:52:51 +0100

    solve issue

--- algorithms/move-zeros-to-the-end.js
+++ algorithms/move-zeros-to-the-end.js
@@ -1,15 +1,21 @@
 function moveZeros(arr){
 
-    let newArr = [];
+    let arrOne = [];
+    let arrTwo = []
+    
     for ( let num of arr){
 
+        if ( num !== 0){
+            arrOne.push(num);
+        }
+
         if ( num === 0){
-            Array.from(arr).replaceAll(num, '')
-            newArr.push(num)
+            arrTwo.push(num)
         }
     }
-    console.log(arr + newArr);
+    console.log(arrOne.concat(arrTwo));
     
 
 }
 moveZeros([false,1,0,1,2,0,1,3,"a"]);
+moveZeros([1,2,0,1,0,1,0,3,0,1])
