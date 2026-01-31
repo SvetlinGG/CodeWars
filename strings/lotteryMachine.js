1527a1ca7e7b185c6fcc2a5829a845fb7674e4f3
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-01-31 10:44:28 +0100

    completed task

--- strings/lotteryMachine.js
+++ strings/lotteryMachine.js
@@ -3,20 +3,28 @@ function lottery(str){
 
     let word = str.split('');
     let arr = []
+    const seen = new Set();
     
     for (let i = 0; i < word.length; i ++){
         
-        if (word[i] >= "0" && word[i] <= "9"){
-            
-            arr.push(word[i])
-        }
+        if (word[i] >= "0" && word[i] <= "9" && !seen.has(word[i])){
+            seen.add(word[i])
+            arr.push(word[i]);
+    }
+
+    }
+    if ( arr.length === 0){
+        console.log('One more run!');
         
     }
-    
     console.log(arr.join(''));
+    
+    
+    
+    
 
 
 }
 lottery('hPrBKWDH8yc6Lt5NQZWQ');
 lottery('ynMAisVpHEqpqHBqTrwH');
-lottery('555')
+lottery('555');
