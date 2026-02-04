a8cd27137e27ab4aaf8f41375e592f4b2003d53e
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-02-04 11:52:14 +0100

    change negative numbers

---
+++ algorithms/change-two-dimensional-array.js
@@ -0,0 +1,16 @@
+function matrix(arr){
+    console.log(arr[1][1]);
+    console.log(arr[2][2]);
+    console.log(arr[3][3]);
+    console.log(arr[4][4]);
+    
+
+
+}
+matrix([
+    [-1,  4, -5, -9,  3 ],
+    [ 6, -4, -7,  4, -5 ],
+    [ 3,  5,  0, -9, -1 ],
+    [ 1,  5, -7, -8, -9 ],
+    [-3,  2,  1, -5,  6 ]
+  ]);
