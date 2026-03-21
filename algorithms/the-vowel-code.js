57fcba2bd6272b291add6b5cec573d5faeb29fa9
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-21 13:46:58 +0100

    vowel algorithm

---
+++ algorithms/the-vowel-code.js
@@ -0,0 +1,19 @@
+function encode(string) {
+    let vowel = {
+        a: 1,
+        e: 2,
+        i: 3,
+        o: 4,
+        u: 5,
+    }
+    for (let char of string) {
+        if ( char. )
+        
+    }
+    
+  }
+  encode("hello")
+  
+  function decode(string) {
+    return
+  }
