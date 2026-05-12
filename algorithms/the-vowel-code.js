a969c288fca5abfc1894112f5c8be1f93063eed2
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-12 15:35:34 +0200

    setup the new string

--- algorithms/the-vowel-code.js
+++ algorithms/the-vowel-code.js
@@ -1,36 +1,47 @@
 function encode(string) {
     let vowel = {
-        a: 1,
-        e: 2,
-        i: 3,
-        o: 4,
-        u: 5,
+        a: '1',
+        e: '2',
+        i: '3',
+        o: '4',
+        u: '5',
     }
     for ( let str of string){
         for ( let char in vowel){
-            if ( str == char ){
+            if ( str == char){
                 string = string.replace(str, vowel[char])
             }
             
         }
     }
     console.log(string);
+    
+    
+  
+  
+  let decode = (string) => {
+    let numbers = {
+        '1': 'a',
+        '2': 'e',
+        '3': 'i',
+        '4': 'o',
+        '5': 'u',
+    }
 
-    let decode = (string) => {
-        for( let char in vowel){
-            for( let str of string){
 
+    for( let str of string){
+        for( let num in numbers){
+
+            if ( str == num ){
+                string = string.replace(str, numbers[num])
+            }
                 
-                    string = string.replace(vowel[char], str)
-                }
             }
         }
-    
-    let newStr = decode(string);
-    console.log(newStr);
-    
-    
-  }
-  encode("hello")
-  
-  
+    }
+
+let newStr = decode(string);
+console.log(newStr);
+
+}
+encode("hello")
