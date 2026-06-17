73ac902c4d1d6b226520dc62f0cbd6ef2cc3b603
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-06-17 14:58:55 +0200

    highest rank

---
+++ arrays/highesRang.js
@@ -0,0 +1,6 @@
+function highestRank(arr){
+//Your Code logic should written here
+}
+highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]);
+highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10] );
+highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]);
