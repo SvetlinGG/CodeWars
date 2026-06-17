606b224a9dced04798f9b90aba55254687ddfc13
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-06-17 21:19:49 +0200

    some changes

--- arrays/highesRang.js
+++ arrays/highesRang.js
@@ -1,6 +1,17 @@
 function highestRank(arr){
-//Your Code logic should written here
+    let rank = {
+        num: 0,
+    }
+
+    for( let num of arr){
+        if (num){
+            rank.num = num;
+            rank[0] ++;
+        }
+    }
+    console.log(rank);
+    
 }
 highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]);
-highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10] );
-highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]);
+//highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10] );
+//highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]);
