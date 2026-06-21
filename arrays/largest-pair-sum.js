de3d5ad5d75d84c3f22ddd867d6461534b25e744
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-06-21 21:59:00 +0200

    how many stairs

--- arrays/largest-pair-sum.js
+++ arrays/largest-pair-sum.js
@@ -6,3 +6,9 @@ function largestPairSum(numbers){
     // let sum = lastNum + prevNum
     // console.log(`${sum} (= ${lastNum} + ${prevNum})`);
     const sorted = [...numbers].sort((a, b) => a - b);
+    console.log(sorted[sorted.length - 1] + sorted[sorted.length - 2]);
+
+
+}
+largestPairSum([10, 14, 2, 23, 19]);
+largestPairSum([99, 2, 2, 23, 19]);
--- arrays/unique-In-Order.js
+++ arrays/unique-In-Order.js
@@ -3,15 +3,4 @@ function solve(str){
     let character = [];
     for ( let i = 0; i < str.length; i++){
         if (str[i] != str[i + 1]){
-            character.push(str[i])
-        }
-        
-    }
-    console.log(character);
-    
-
-
-}
-solve('AAAABBBCCDAABBB');
-solve('ABBCcAD');
-solve([1,2,2,3,3])
+            ch
