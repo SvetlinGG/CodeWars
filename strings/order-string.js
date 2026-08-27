89fbd741cced6b855eb88233c134f4607d6daf61
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-27 12:47:35 +0200

    complete task

--- strings/order-string.js
+++ strings/order-string.js
@@ -1,4 +1,16 @@
 function solve(words){
+    
+    
+    let text = words.split(' ');
+    text.sort((a, b) => {
+      let numA = a.match(/\d/g);
+      let numB = b.match(/\d/g);
+
+      return numA - numB;
+    })
+  
+    console.log(text.join(' '));
+    
 
     
       
